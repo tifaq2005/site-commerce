09b8907872d681d68e05527ce538e375eb33c6b4
tifaq2005 <tifaq2005@gmail.com>
2023-05-07 23:06:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let Home =document.getElementById("Home");
+let imgg =document.getElementById("imgg");
+
+
+
+Home.addEventListener("click",function entrerHome(){
+    imgg.style.display="none";
+});
+
+
